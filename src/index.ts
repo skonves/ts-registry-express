ca96a56f9b519eb63cabd58fd58f5169565bcb39
Steve Konves <stephen.konves@gmail.com>
2019-10-26 12:06:29 -0700

    feat: create scope provider

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import { Request, Response, NextFunction } from 'express';
+import { ScopeProvider, singleton } from 'ts-registry';
+
+import * as httpContext from 'express-http-context';
+
+export const request: ScopeProvider<Request> = {
+  getTargetScope: () => httpContext.get('express_req'),
+  sourceScopeGetters: [
+    () => httpContext.get('express_req'),
+    ...singleton.sourceScopeGetters,
+  ],
+};
+
+export const middleware = [
+  httpContext.middleware,
+  (req: Request, _res: Response, next: NextFunction) => {
+    httpContext.set('express_req', req);
+    next();
+  },
+];
